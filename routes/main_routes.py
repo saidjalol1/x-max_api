09a826ec8e00a6b3e1ac64da71e8b88dbe4fc31e
Saidjalol Turakhujayev <turakhujayevsaidjalol06@gmail.com>
2024-01-23 09:31:27 +0500

    query changed

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -4,7 +4,7 @@ from utils import verify_api_key, get_or_create_user_token
 from config import get_db
 from models import models, schemas
 
-from typing import List
+from typing import List, Optional
 
 
 main_routes = APIRouter(
@@ -16,9 +16,9 @@ main_routes = APIRouter(
 
 @main_routes.get("/")
 async def index(
+    name: Optional[str],
     api_key : str = Depends(verify_api_key),
     db: Session = Depends(get_db),
-    name: str = None,
 ):
     if name == None:
         products = db.query(models.Product).options(joinedload(models.Product.category)).options(joinedload(models.Product.images)).all()
