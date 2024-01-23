597423c52a53d477032328bfcacc87ae06149b41
Saidjalol Turakhujayev <turakhujayevsaidjalol06@gmail.com>
2024-01-23 09:42:34 +0500

    query changed again

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -4,7 +4,7 @@ from utils import verify_api_key, get_or_create_user_token
 from config import get_db
 from models import models, schemas
 
-from typing import List, Optional
+from typing import List, Optional, Query
 
 
 main_routes = APIRouter(
@@ -16,15 +16,16 @@ main_routes = APIRouter(
 
 @main_routes.get("/")
 async def index(
-    name: Optional[str],
+    name: str = Query(None, alias="name"),
     api_key : str = Depends(verify_api_key),
     db: Session = Depends(get_db),
 ):
-    if name == None:
-        products = db.query(models.Product).options(joinedload(models.Product.category)).options(joinedload(models.Product.images)).all()
+    if name:
+        products = db.query(models.Product).options(joinedload(models.Product.category)).options(joinedload(models.Product.images)).filter(models.Product.name.contains(name))
         categories = db.query(models.Category).all()
     else:
         products = db.query(models.Product).options(joinedload(models.Product.category)).options(joinedload(models.Product.images)).filter(models.Category)
+        categories = db.query(models.Category).all()
     respons = {
         "products": products,
         "categories": categories
